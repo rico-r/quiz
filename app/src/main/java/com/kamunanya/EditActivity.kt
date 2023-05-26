5783fb46ee5972982fdb719b03a5884a2228d998
Rico Ronaldo <91813137+rico-r@users.noreply.github.com>
2023-05-26 08:07:06 +0700

    Enable navigation in edit activity

--- app/src/main/java/com/kamunanya/EditActivity.kt
+++ app/src/main/java/com/kamunanya/EditActivity.kt
@@ -2,10 +2,20 @@ package com.kamunanya
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 
 class EditActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit)
+        setContentView(R.layout.activity_quiz)
+
+        val navController = this.findNavController(R.id.editNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this,navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.editNavHostFragment)
+        return NavigationUI.navigateUp(navController, null)
     }
 }
